034c92b4da013ea9023a4195cfc3b37967286c92
Abduraimbek <yarkinovabduraimbek@gmail.com>
2022-12-04 12:36:25 +0300

    Bloc Counter

--- bloc_counter/lib/counter/counter_page.dart
+++ bloc_counter/lib/counter/counter_page.dart
@@ -1,10 +1,46 @@
+import 'package:bloc_counter/counter/bloc/counter_bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class CounterPage extends StatelessWidget {
-  const CounterPage({super.key});
+  const CounterPage({
+    super.key,
+    required this.title,
+  });
+
+  final String title;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'You have pushed the button this many times:',
+              textAlign: TextAlign.center,
+            ),
+            BlocBuilder<CounterBloc, CounterState>(
+              builder: (context, state) {
+                return Text(
+                  '${(state as CounterCountState).count}',
+                  style: Theme.of(context).textTheme.headline4,
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () =>
+            context.read<CounterBloc>().add(CounterIncrementPressed()),
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
   }
 }
